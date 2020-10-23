56ad1b54b8a565ece2666cb27476045f2179c7d9
Desiree Lerma <desi.lerma96@yahoo.com>
2020-10-22 22:09:49 -0500

    commit tree 4785662b82161a14ecb9128643988f42c48cd6aa

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length >= 3 
+      last = array.pop
+      array.push("and "+last)
+      array.join(", ")
+    elsif array.length >= 1
+      array.join(" and ")  
+    else
+      array.join  
+    end  
 end
